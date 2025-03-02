6aeed9b4fc185e8f93d8f7a7fffd8ea910b8e0ec
hadyAdly <128036967+hadyAdly@users.noreply.github.com>
2025-03-02 10:28:51 +0200

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,277 @@
+﻿create table Users(
+user_id int identity,
+Username varchar(100),
+Password varchar(100),
+Email varchar(100),
+Role varchar(100),
+Phone_number int,
+Primary key(user_id)
+);
+create table Lecturer(
+Lecturer_id int identity,
+schedule varchar(100),
+Primary key(Lecturer_id),
+foreign key(Lecturer_id) references Users
+);
+create table LecturerFields(
+Lecturer_id int identity,
+field varchar(100),
+Primary key(Lecturer_id,field),
+foreign key(Lecturer_id) references Lecturer 
+);
+create table Company(
+Company_id int identity,
+Name varchar(100),
+Representative_name varchar(100),
+Representative_Email varchar(100),
+Location varchar(100),
+Primary key(Company_id),
+foreign key(Company_id) references Users 
+);
+create table Employee(
+Staff_id int identity,
+Company_id int identity,
+Username varchar(100),
+Password varchar(100),
+Email varchar(100),
+Field varchar(100),
+Phone int,
+Primary key(Staff_id,Company_id),
+foreign key(Company_id) references Company
+);
+create table External_Examiner(
+EE_id int identity,
+Schedule varchar(100),
+Primary key(EE_id),
+foreign key(EE_id) references Users
+);
+create table Teaching_Assistant(
+TA_id int identity,
+Schedule varchar(100),
+Primary key(TA_id),
+foreign key(TA_id) references Users
+);
+create table Coordinator(
+coordinator_id int identity,
+Primary key(coordinator_id),
+foreign key(coordinator_id) references Users
+);
+create table Student(
+sid int identity,
+first_name varchar(100),
+last_name varchar(100),
+Major_code int,
+Date_Of_Birth datetime,
+Adress varchar(100),
+Age AS (YEAR(CURRENT_TIMESTAMP) - YEAR(Date_Of_Birth)),
+Semester int,
+GPA float,
+TotalBachelorGrade AS((0.3*Thesis.Total_grade+0.3*Defense.Total_grade+ 0.4*ComulativeProgressReportGrade)),
+Assigned_Project_code int,
+Primary key(sid),
+foreign key(sid) references Users,
+foreign key(Major_code) references Major,
+foreign key(Assigned_Project_code) references Bachelor_Project
+);
+create table Bachelor_Project(
+Code int,
+Name varchar(100),
+Submitted_Materials varchar(100),
+Description varchar(100),
+Primary key(Code)
+);
+create table BachelorSubmittedMaterials(
+Code int,
+Material varchar(100),
+Primary key(Code),
+foreign key(Code) references Bachelor_Project
+);
+create table Academic(
+Academic_code int,
+L_id int identity,
+TA_id int identity,
+EE_id int identity,
+Primary key(Academic_code),
+foreign key(Academic_code) references Bachelor_Project,
+foreign key(L_id) references Lecturer,
+foreign key(TA_id) references Teaching_Assistant,
+foreign key(EE_id) references External_Examiner
+);
+create table Industrial(
+Industrial_code int,
+C_id int identity,
+L_id int identity,
+E_id int identity,
+Primary key(Industrial_code),
+foreign key(Industrial_code) references Bachelor_Project,
+foreign key(C_id) references Company,
+foreign key(L_id) references Lecturer,
+foreign key(E_id) references Employee
+);
+create table Faculty(
+Faculty_Code int,
+Name varchar(100),
+Dean varchar(100),
+Primary key(Faculty_Code),
+foreign key(Dean) references Lecturer
+);
+create table Major(
+Major_Code int,
+Faculty_code int,
+Major_Name varchar(100),
+Primary key(Major_Code),
+foreign key(Faculty_code) references Faculty
+);
+create table Meeting(
+Meeting_ID int identity,
+L_id int identity,
+STime float,
+ETime float,
+Duration As (Meeting.ETime – Meeting.STime),
+Date datetime,
+Meeting_Point varchar(100),
+Primary key(Meeting_ID),
+foreign key(L_id) references Lecturer
+);
+create table MeetingToDoList(
+Meeting_ID int identity,
+ToDoList varchar(100),
+Primary key(Meeting_ID,ToDoList),
+foreign key(Meeting_ID) references Meeting
+);
+create table MeetingAttendents(
+Meeting_ID int identity,
+Attendant_id int identity,
+Primary key(Meeting_ID,Attendant_id),
+foreign key(Meeting_ID) references Meeting,
+foreign key(Attendant_id) references Users
+);
+create table Thesis(
+sid int identity,
+Title varchar(100),
+Deadline datetime,
+PDF_doc varchar(100),
+Total_grade As ((GradeAcademicThesis.EE_grade+GradeAcademicThesis.Lecturer_grade)/2 or 
+(GradeIndustrialThesis.Company_grade And GradeIndustrialThesis.Employee_grade)/2),
+Primary key(sid,Title),
+foreign key(sid) references Student
+);
+create table Defense(
+sid int identity,
+Location varchar(100),
+Content varchar(100),
+Time time,
+Date datetime,
+Total_Grade As ((GradeAcademicDefense.EE_grade + GradeAcademicDefense.Lecturer_grade)/2 
+or (GradeIndustrialDefense.Compay_grade+GradeIndustrialDefense.Employee_grade)/2),
+Primary key(sid,Location),
+foreign key(sid) references Student
+);
+create table ProgressReport(
+sid int identity,
+Date datetime,
+Content varchar(100),
+Grade AS (GradeAcademicPR.Lecturer_grade or GradeIndustrialPR.Company_grade),
+UpdatingUser_id int identity,
+ComulativeProgressReportGrade AS(Average ProgressReprt.grade),
+Primary key(sid,Date),
+foreign key(sid) references Student,
+foreign key(UpdatingUser_id) references Users
+);
+create table GradeIndustrialPR(
+C_id int identity,
+sid int identity,
+Date datetime,
+Company_grade float,
+Lecturer_grade float,
+Primary key(sid,Date),
+foreign key(C_id) references Company,
+foreign key(sid) references ProgressReport,
+foreign key(Date) references ProgressReport
+);
+create table GradeAcademicPR(
+L_id int identity,
+sid int identity,
+Date datetime,
+Lecturer_grade float,
+Primary key(sid,Date),
+foreign key(L_id) references Lecturer,
+foreign key(sid) references ProgressReport,
+foreign key(Date) references ProgressReport
+);
+create table GradeAcademicThesis(
+L_id int identity,
+EE_id  int identity,
+sid int identity,
+Title varchar(100),
+EE_grade float,
+Lecturer_grade float,
+Primary key(sid,Title),
+foreign key(L_id) references Lecturer,
+foreign key(EE_id) references External_Examiner,
+foreign key(sid) references Thesis,
+foreign key(Title) references Thesis
+);
+create table GradeIndustrialThesis(
+C_id int identity,
+E_id int identity,
+sid int identity,
+Title varchar(100),
+Company_grade float,
+Employee_grade float,
+Primary key(sid,Title),
+foreign key(C_id) references Company,
+foreign key(E_id) references Employee,
+foreign key(sid) references Thesis,
+foreign key(Title) references Thesis
+);
+create table GradeAcademicDefense(
+L_id int identity,
+EE_id int identity,
+sid int identity,
+Location varchar(100),
+EE_grade float,
+Lecturer_grade float,
+Primary key(sid,Location),
+foreign key(L_id) references Lecturer,
+foreign key(EE_id) references External_Examiner,
+foreign key(sid) references Defense,
+foreign key(Location) references Defense
+);
+create table GradeIndustrialDefense(
+C_id int identity,
+E_id int identity,
+sid int identity,
+Location varchar(100),
+Company_grade float,
+Employee_grade float,
+Primary key(sid,Location),
+foreign key(C_id) references Company,
+foreign key(E_id) references Employee,
+foreign key(sid) references Defense,
+foreign key(Location) references Defense
+);
+create table LecturerRecommendEE(
+L_id int identity,
+EE_id int identity,
+PCode int,
+Primary key(EE_id,PCode),
+foreign key(L_id) references Lecturer,
+foreign key(EE_id) references External_Examiner,
+foreign key(PCode) references Academic
+);
+create table StudentPreferences(
+sid int identity,
+PCode int,
+PreferenceNumber int,
+Primary key(sid,PCode),
+foreign key(sid) references Student,
+foreign key(PCode) references Bachelor_Project
+);
+create table MajorHasBachelorProject(
+Major_code int,
+Project_code int,
+Primary key(Major_code,Project_code),
+foreign key(Major_code) references Major,
+foreign key(Project_code) references Bachelor_Project
+);
---
+++ SqlQuery_1no (1).sql
@@ -0,0 +1,129 @@
+﻿CREATE proc AddEmployee
+@CompanyID int output,@email varchar(50),@name varchar(20),@phone_number varchar(20),@field varchar(25),
+@StaffID int output,
+@pass varchar(10) output
+AS
+IF (SELECT Company_id FROM Company WHERE Company_id=@CompanyID ) is not null
+begin
+set @pass= @StaffId
+select @StaffID =max(staff_id) from Employee
+INSERT into Employee(staff_id,Company_id,Username,Password,Email,Field,Phone)
+VALUES(@StaffID,@CompanyID,@name,@pass,@email,@field,@phone_number)
+
+end
+ELSE
+PRINT 'there is no company ny that name';
+go
+/*to be investigated......detective conan ys3a da2eman*/
+CREATE proc CompanyCreateLocalProject
+@company_id int,@proj_code varchar(10),@title varchar(50),@description varchar(200),@major_code varchar(10)
+AS
+INSERT into Bachelor_Project(Name,Description)
+VALUES(@title,@description)
+INSERT into MajorHasBachelorProject(Major_code,Project_code)
+VALUES (@major_code,@proj_code)
+INSERT into Industrial(Industrial_code,C_id)
+VALUES(@proj_code,@company_id)
+go
+CREATE proc AssignEmployee
+@bachelor_code varchar(10),@staff_id int output, @Company_id int
+AS
+UPDATE Industrial 
+SET E_id=@staff_id
+WHERE C_id=@Company_id and Industrial_code=@bachelor_code
+go
+/*grades are to be revised since they don't make any utter sense rn*/
+CREATE proc CompanyGradeThesis
+@Company_id int,@sid int ,@date datetime,@Company_grade Decimal(4,2),@thesis_title varchar(50)
+AS
+UPDATE Thesis
+SET Total_Grade =(GradeIndustrialThesis.grade&GradeIndustrialThesis.Employee_grade)/2
+WHERE sid =@sid and Title= @thesis_title
+go
+CREATE proc CompanyGradeDefense
+@Company_id int,@sid int ,@date datetime,@Company_grade Decimal(4,2),@defense_location varchar(5)
+AS
+UPDATE Defense
+SET Total_Grade =(GradeIndustrialDefense.Compay_grade+GradeIndustrialDefense.Employee_grade)/2
+WHERE sid =@sid and Location= @defence_location
+go
+CREATE proc CompanyGradePR
+@Company_id int ,@sid int , @date datetime,@Company_grade decimal(4,2)
+AS
+UPDATE ProgressReport 
+SET ComulativeProgressReportGrade = calculated(Average ProgressReprt.grade),Grade= Calculated(GradeAcademicPR.Lecturer_grade or
+GradeIndustrialPR.Company_grade)
+WHERE sid=@sid and Date =date
+go
+CREATE proc TACreatePR
+@TA_id int ,@sid int ,@date datetime,@content varchar(1000)
+AS
+INSERT into ProgressReport(sid,Date,Content)
+VALUES(@sid,@date,@content)
+go
+CREATE proc TAAddToDO
+@meeting_id int ,@to_do_list varchar(200)
+AS
+UPDATE MeetingToDoList
+SET ToDoList= @to_do_list
+WHERE Meeting_ID =@meeting_id
+go
+CREATE proc ViewUsers
+@User_type varchar(20),@User_id int
+AS
+IF @User_type is not null and @User_id is not null
+begin
+SELECT* From Users
+WHERE user_id=@User_id and Role=@User_type
+end
+ELSE
+IF @User_id is null
+begin
+SELECT* FROM Users
+WHERE Role=@User_type
+end
+ELSE
+begin
+SELECT* FROM Users
+WHERE user_id=@User_id
+end
+go
+CREATE proc AssignAllStudentsToLocalProject
+AS
+SELECT Student.sid,Bachelor_Project.*
+FROM Student INNER JOIN Bachelor_Project
+ON Student.Assigned_Project_code=Bachelor_Project.Code
+go
+/*why the fuck does it give coordinator id*/
+CREATE proc AssignTAs
+@coordinator_id int ,@TA_id int ,@proj_code varchar(10)
+UPDATE Bachelor_Project
+SET 
+WHERE
+go
+CREATE proc ViewRecommendation
+@lecture_id int 
+AS
+IF @lecture_id is null
+SELECT* FROM LecturerRecommendEE
+ELSE
+BEGIN
+SELECT* FROM LecturerRecommendEE
+WHERE L_id=@lecture_id
+END
+go
+CREATE proc AssignEE
+@coordinator_id int , @EE_id int ,@proj_code varchar(10)
+AS
+IF (SELECT PCode FROM LecturerRecommendEE)=@proj_code and (SELECT EE_id FROM LecturerRecommendEE)=@EE_id
+begin
+UPDATE Academic
+SET EE_id=@EE_id
+WEHRE Academic_code=@proj_code
+go 
+CREATE proc ScheduleDefense
+@sid int ,@data datetime,@time time,@location varchar(5)
+AS
+INSERT into Defense(sid,Location,Time,Date)
+VALUES(@sid,@location,@time,@data)
+go
